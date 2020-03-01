fcb003b19592518920e71671bfb2ecb2307006e4
Miss Mercy <miss.mercyyy@gmail.com>
2020-03-01 14:11:40 +0100

    Fix filenames as well

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Fix space in path
 
-Windows cannot deal with directory names that start or end with a whitespace. When renaming them in Windows Explorer,
-spaces are stripped without informing the user. However if other programs create directories with such malformed names,
-Windows Explorer cannot delete or rename them anymore. This can happen for example when unpacking a file archive.
-
-This script scans directories recursively, and offers to remove the spaces.
+Some programs cannot deal with directory or file names, that start or end with a whitespace. File explorers usually fix 
+this on the fly, but other applications might still create such malformed names. This can happen for example when 
+unpacking a file archive. This script scans directories recursively, and offers to remove the spaces.
 
 ## Dependencies
 
@@ -17,10 +15,15 @@ Set the directory you want to scan inside the script, then execute without argum
 
 Example output:
 
-     BROKEN: "C:\path\ dir\"
+     BROKEN: "C:\path\ dir \"
         FIX: "C:\path\dir\"
     Rename this? y
     Fixed.
+
+     BROKEN: "C:\path\ file . jpg "
+        FIX: "C:\path\file.jpg"
+    Rename this? n
+    Not fixed.
 
 ## Author and license
 
--- fix_space_in_path.py
+++ fix_space_in_path.py
@@ -6,16 +6,26 @@ RECURSIVE_FIX_DIRECTORY_NAMES_HERE = 'C:\\path'
 def main() -> None:
     for root, directories, files in os.walk(RECURSIVE_FIX_DIRECTORY_NAMES_HERE):
         for directory in directories:
-            if not (directory.startswith(' ') or directory.endswith(' ')):
-                continue
-            broken = os.path.join(root, directory) + os.sep
-            fixed = os.path.join(root, directory.strip(' ')) + os.sep
-            print(f' BROKEN: "{broken}"\n    FIX: "{fixed}"')
-            if input('Rename this? ') == 'y':
-                os.rename(broken, fixed)
-                print('Fixed.\n')
-            else:
-                print('Not fixed.\n')
+            rename(broken=os.path.join(root, directory) + os.sep,
+                   fixed=os.path.join(root, directory.strip(' ')) + os.sep)
+        for file in files:
+            name, extension = os.path.splitext(file)
+            rename(broken=os.path.join(root, file),
+                   fixed=os.path.join(root, name.strip(' ') + extension.strip(' ')))
+
+
+def rename(
+        broken: str,
+        fixed: str
+) -> None:
+    if broken == fixed:
+        return
+    print(f' BROKEN: "{broken}"\n    FIX: "{fixed}"')
+    if input('Rename this? ') == 'y':
+        os.rename(broken, fixed)
+        print('Fixed.\n')
+    else:
+        print('Not fixed.\n')
 
 
 if __name__ == '__main__':
